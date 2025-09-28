98dd072141d331d635b914ca1c326357368550f1
jumayev54 <abcjumayev@gmail.com>
2025-09-28 01:48:08 -0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",           // <-- changed
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
