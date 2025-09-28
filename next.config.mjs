d9de1ac402bbfcdea92d7391e8aac131efbfb8e5
jumayev54 <abcjumayev@gmail.com>
2025-09-28 01:54:16 -0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/syllabus-manament-app" : "",
-  assetPrefix: isProd ? "/syllabus-manament-app/" : "",
 };
 
 export default nextConfig;
